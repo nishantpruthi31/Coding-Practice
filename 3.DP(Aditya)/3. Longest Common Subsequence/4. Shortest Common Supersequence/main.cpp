378bf9c0d8d48b17a4ad4a09e319c59360d02164
Nishant Pruthi <nishantpruthi12371@gmail.com>
2022-07-21 18:35:49 +0530

    Shortest Common Supersequence problem on LCS

---
+++ 3.DP(Aditya)/3. Longest Common Subsequence/4. Shortest Common Supersequence/main.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string x;
+   string y;
+   
+   cout<<"Enter strings"<<endl;
+   cin>>x>>y;
+   
+   int m=x.size();
+   int n=y.size();
+   
+   vector<vector<int>> dp(m+1,vector<int> (n+1));          // dp array 
+   
+   
+   // m==i n==j
+   
+   for(int i=0;i<m+1;i++)
+   {
+       for(int j=0;j<n+1;j++)
+       {
+           if(i==0 || j==0)         // initilize 1st row and 1 st column with 0
+           dp[i][j]=0;
+           
+           else if(x[i-1]==y[j-1])
+           {
+               dp[i][j]=1+dp[i-1][j-1];
+           }
+           else
+           {
+               dp[i][j]= max(dp[i][j-1],dp[i-1][j]);
+           }
+       }
+   }
+   
+   cout<<"Length of longest common-subsequence: "<<dp[m][n]<<endl;            
+   
+   
+   cout<<"Longest  super-sequence is: "<<(m+n)-dp[m][n]<<endl;
+   
+    return 0;
+}
